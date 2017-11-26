4886b587bce8acff026bd2a9873f3e6c1a232947
unknown <pmlhtoosvsyekm@gmail.com>
2017-11-27 00:04:30 +0800

    Add homework assignment.

---
+++ src/main/java/idv/workshop/homework/Cat.java
@@ -0,0 +1,26 @@
+package idv.workshop.homework;
+
+public class Cat {
+
+    /*
+     * Description:
+     * A cat object should has following fields(attribute / instance variable)
+     *
+     * 1. name: String type
+     * 2. age: int type
+     * 3. weight: int type
+     * 4. color: String type
+     * 5. gender: String
+     *
+     * Request:
+     * Please complete this class so that this class is a well-defined POJO.
+     *
+     * 1. I can create a cat object via the constructor method,
+     *    and the constructor should accept all the above mentioned fields.
+     *
+     * 2. Please write unit tests to verified that all the passed-in fields
+     *    are correct when I get them from getter method.
+     *
+     */
+
+}
