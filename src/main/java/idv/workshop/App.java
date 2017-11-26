bcc4ddce698c6c4309cd86534df5c07d1edef11f
unknown <pmlhtoosvsyekm@gmail.com>
2017-11-26 22:38:06 +0800

    First commit

---
+++ src/main/java/idv/workshop/App.java
@@ -0,0 +1,13 @@
+package idv.workshop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/idv/workshop/string/StringTestDemo1.java
@@ -0,0 +1,9 @@
+package idv.workshop.string;
+
+public class StringTestDemo1 {
+
+    public static void main(String[] args) {
+        // sout + tab
+        System.out.println("It's a test string.");
+    }
+}
