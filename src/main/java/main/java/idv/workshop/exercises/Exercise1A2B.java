20eb636736aaddafdf1d38c2fef98fade4c63df3
unknown <pmlhtoosvsyekm@gmail.com>
2019-05-20 03:30:28 +0800

    Exercise 1A2B

--- src/main/java/main/java/idv/workshop/exercises/Exercise1A2B.java
+++ src/main/java/main/java/idv/workshop/exercises/Exercise1A2B.java
@@ -1,4 +1,100 @@
 package main.java.idv.workshop.exercises;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Exercise1A2B {
+
+    private List ans = new ArrayList();
+    private static int getA;
+
+    public void answer() {
+
+        int rnd;
+
+        while (ans.size() < 4) {
+            rnd = (int)(Math.random() * 10);
+
+            if(ans.indexOf(rnd) == -1) {
+                ans.add(rnd);
+            }
+        }
+
+    }
+
+    public void checkAns(List keyIn) {
+
+        int getB = 0;
+
+        for(int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+
+                if(keyIn.get(i) == ans.get(j)) {
+
+                    if(i == j) {
+                        getA++;
+                    } else {
+                        getB++;
+                    }
+                }
+            }
+        }
+
+        if(getA < 4) {
+            System.out.println("Result: " + getA + "A" + getB + "B");
+        } else {
+            System.out.println("Congratulation!!!");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+        String in;
+        List keyIn = new ArrayList();
+        Exercise1A2B dp = new Exercise1A2B();
+        dp.answer();
+
+        z:while (getA < 4) {
+
+            keyIn.clear();
+            getA = 0;
+
+            sum = sum + 1;
+            System.out.println("Please enter four numbers. (not repeated) times: " + sum );
+            Scanner sc = new Scanner(System.in);
+            in = sc.next();
+
+
+            if(in.length() != 4) {
+
+                sum = sum - 1;
+
+                System.out.println("Enter error");
+                continue z;
+
+            } else {
+
+                for(int i = 0; i < 4; i++) {
+
+                    int key = Character.getNumericValue(in.charAt(i));
+
+                    if(keyIn.indexOf(key) == -1) {
+                        keyIn.add(key);
+
+                    } else {
+
+                        sum = sum - 1;
+
+                        System.out.println("Number is repeated");
+                        continue z;
+                    }
+                }
+
+                dp.checkAns(keyIn);
+
+            }
+        }
+    }
 }
